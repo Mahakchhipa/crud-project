60d235d7bb7b896c5f390f8b7d8ba74a11fe740e
Mahakchhipa <mehekjajpuraom700@gmail.com>
2025-04-16 18:14:55 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center justify-center py-5 text-4xl bg-pink-300 font-bold ">
+        {" "}
+        My React Project{" "}
+      </h1>
+    </div>
+
+  );
+};
+
+export default App;
